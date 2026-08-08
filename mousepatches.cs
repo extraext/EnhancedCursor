7b998d012925171ca068099c8630da4af04d1351
extraext <ahdalbagory@gmail.com>
2026-08-09 00:07:10 +0300

    Update mousepatches.cs

--- mousepatches.cs
+++ mousepatches.cs
@@ -8,24 +8,31 @@ namespace EnhancedCursor
     {
         [HarmonyPatch("SetCursor", typeof(Texture2D), typeof(Vector2), typeof(CursorMode))]
         [HarmonyPrefix]
-        public static void SetCursor_Prefix(ref Texture2D texture, ref Vector2 hotspot, ref CursorMode cursorMode)
+        public static bool SetCursor_Prefix(ref Texture2D texture, ref Vector2 hotspot, ref CursorMode cursorMode)
         {
+            if (EnhancedCursorAddon.Instance != null && EnhancedCursorAddon.Instance.IsPanning)
+            {
+                return false; 
+            }
+
             if (CursorSettings.ModEnabled && CursorSettings.EnableCustomCursor && CursorSettings.HasActiveCursor && CursorSettings.IsActiveInCurrentScene())
             {
-                if (CursorSettings.ActiveCursorItem.Texture != null)
+                var activeItem = CursorSettings.ActiveCursorItem;
+                if (activeItem.Texture != null)
                 {
-                    texture = CursorSettings.ActiveCursorItem.Texture;
+                    texture = activeItem.Texture;
                     hotspot = new Vector2(CursorSettings.HotspotX, CursorSettings.HotspotY);
                     cursorMode = CursorMode.Auto;
                 }
             }
+            return true;
         }
 
         [HarmonyPatch("visible", MethodType.Setter)]
         [HarmonyPrefix]
         public static bool set_visible_Prefix(ref bool value)
         {
-            if (CursorSettings.ModEnabled && CursorSettings.EnableCustomCursor && CursorSettings.HasActiveCursor && CursorSettings.IsActiveInCurrentScene())
+            if (CursorSettings.ModEnabled && CursorSettings.IsActiveInCurrentScene())
             {
                 if (EnhancedCursorAddon.Instance != null && EnhancedCursorAddon.Instance.ShouldSuppressHide())
                 {
